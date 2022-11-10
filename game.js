93c852e50530f835032b99280a161bfc0fc66a47
FrancesOBrien <114510060+FrancesOBrien@users.noreply.github.com>
2022-11-10 11:42:30 -0500

    guesses populate with click on color

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+//global variable for guesses
+let guesses = []
+let guess = [] 
+let guessIndex = 0
+
 //generate code one time for entire game
 //function getCode will generate an array of 4 randomly from this array, include duplicates
 const colors = ['red', 'yellow', 'green', 'blue', 'black', 'white']
@@ -17,8 +22,8 @@ let secret = getCode(colors)
 
 //demo round, we'll enter code and guess to check our feedback
 // const code = ['white', 'green', 'yellow', 'red']
-const guess = ['white', 'green', 'yellow', 'red']
-const guessCount = 0
+
+let guessCount = 0
 //every time user enters guess, guessCount++
 //if guessCount == 10, game over
 
@@ -67,32 +72,61 @@ function checkWin(feedback){
 //checkWin called on each guess
 
 //Reveal code
-
+//calculate guess
+let calculateGuess = () => {
+    guesses[guessIndex] = guess
+    console.log(guesses)
+    updateGuessDOM()
+    if (guess.length >= 4){
+        guess = []
+        guessIndex++
+    } 
+}
+let updateGuessDOM = () => {
+    for (let i = 0; i < guesses.length; i++){
+        for (let j = 0; j < guess.length; j++){
+            guessElements[i][j].style.backgroundColor = guesses[i][j]
+            guessElements[i][j].style.border = guesses[i][j]
+        }
+    }
+}
 
 
 //listen for click on color buttons, turn next open circle corresponding color!
 
 const blackBtn = document.getElementById("button-black");
 blackBtn.addEventListener('click', () => {
-    console.log("black button clicked")
+    guess.push("black")
+    calculateGuess()
 })
 const blueBtn = document.getElementById("button-blue");
 blueBtn.addEventListener('click', () => {
-    console.log("blue button clicked")
+    guess.push("rgb(40, 23, 169)")
+    calculateGuess()
 })
 const yellBtn = document.getElementById("button-yellow");
 yellBtn.addEventListener('click', () => {
-    console.log("yellow button clicked")
+    guess.push("rgb(236, 217, 14)")
+    calculateGuess()
 })
 const redBtn = document.getElementById("button-red");
 redBtn.addEventListener('click', () => {
-    console.log("red button clicked")
+    guess.push("orangered")
+    calculateGuess()
 })
 const greenBtn = document.getElementById("button-green");
 greenBtn.addEventListener('click', () => {
-    console.log("green button clicked")
+    guess.push("rgb(30, 179, 57)")
+    calculateGuess()
 })
 const whiteBtn = document.getElementById("button-white");
 whiteBtn.addEventListener('click', () => {
-    console.log("white button clicked")
+    guess.push("lightgoldenrodyellow")
+    calculateGuess()
 })
+
+let circles = Array.from(document.querySelectorAll(".guess"))
+let guessElements = circles.map(el => {
+    return el.children
+})
+console.log(guessElements)
